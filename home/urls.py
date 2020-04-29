b7378d15343c43496897b3c417a0734e4a7d5b66
Flavio Seixas <flavio_seixas@id.uff.br>
2020-04-29 18:10:56 -0300

    primeiro commit

---
+++ home/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import path
+from .views import home, my_logout
+
+
+urlpatterns = [
+    path('', home, name="home"),
+    path('logout/', my_logout, name="logout")
+]
+
